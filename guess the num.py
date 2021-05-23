a37483558ca2a05b883c67a8218a661f15db6e58
Sandra Nasasiira <sandranasasiiraresty@gmail.com>
2021-05-23 15:29:04 +0300

    first commit

---
+++ guess the num.py
@@ -0,0 +1,16 @@
+#guess game where the computer is going to tell you if the number is right or no.so you can guess as many times.
+#import random helps us to inform the computer to give us a random number for our game.
+#import random
+
+#def guess(x):
+    #random_number=random.randint(1,x)
+    #guess=0
+    #   guess= int(input(f'Guess a number between 1 and {x};'))
+      #  if guess < random_number:
+      #      print("Ooops,try again.Too low")
+       # elif guess > random_number:
+       #         print("sorry,that's too high. Almost there!")
+
+
+#guess(14)
+
---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#I created a madlib. It's about joining adjectives and verbs to come up with a meaningful sentence.
+#string concatenation aka(puting strings together)
+#I will use an f string
+adj = input ("adjective:")# in put any adjective eg(nice)
+verb1= input("verb:")# a verb like(discovering)
+verb2= input("verb:")#name of the variable
+famous_person= input("famous person:")#prompt
+madlib=f"coding is so {adj}!  it makes me excited all the time because I love  {verb1}. Stay focused and don't forget to {verb2}. At the end you will fly high like {famous_person} ! "
+print(madlib)
